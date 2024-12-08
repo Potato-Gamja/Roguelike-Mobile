9bddada2d8c213bc53a60ef4c483954c04811fa0
Potato-Gamja <90387670+Potato-Gamja@users.noreply.github.com>
2024-12-08 19:07:18 +0900

    Update FloatingJoystick.cs

--- Scripts/JoystickScirpt/FloatingJoystick.cs
+++ Scripts/JoystickScirpt/FloatingJoystick.cs
@@ -1,11 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class FloatingJoystick : Joystick
+public class FloatingJoystick : Joystick        //Joystick Pack 에셋의 조이스틱 베이스
 {
-    int activePointerId_ = -1;                //중복터치를 방지하기 위한 포인터 아이디
+    int activePointerId_ = -1;                  //중복터치를 방지하기 위한 포인터 아이디
     
     protected override void Start()
     {
@@ -15,7 +12,7 @@ public class FloatingJoystick : Joystick
 
     public override void OnPointerDown(PointerEventData eventData)
     {
-        if (activePointerId_ == -1)            //중복터치를 방지하기 위한 조건문
+        if (activePointerId_ == -1)             //중복터치를 방지하기 위한 조건문
         {
             activePointerId_ = eventData.pointerId;
             background.anchoredPosition = ScreenPointToAnchoredPosition(eventData.position);
