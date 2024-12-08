ba7eff8d76e2fb3efc88810ec0cdff71f0fb20ab
Potato-Gamja <90387670+Potato-Gamja@users.noreply.github.com>
2024-12-08 19:04:36 +0900

    Update PlayerData.cs

--- Scripts/DataScript/PlayerData.cs
+++ Scripts/DataScript/PlayerData.cs
@@ -1,6 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "PlayerData", menuName = "Scriptble Object/PlayerData")]
