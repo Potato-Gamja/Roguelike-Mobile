550e4343a9b550c20217ea53c58464a22e2f5ce2
Potato-Gamja <90387670+Potato-Gamja@users.noreply.github.com>
2024-12-08 18:22:17 +0900

    Update MonsterData.cs

--- Scripts/DataScript/MonsterData.cs
+++ Scripts/DataScript/MonsterData.cs
@@ -7,13 +7,17 @@ using UnityEngine;
 
 public class MonsterData : ScriptableObject
 {
-    public float hp;
-    public float waveHp;
-    public float defense;
-    public int damage;
-    public float offense;
-    public float speed;
-    public float knockbackDefense;
-    public float knockbackTime;
-
+    public float hp;                    //몬스터의 체력
+    
+    public float waveHp;                //몬스터의 웨이브 추가 체력
+    
+    public float defense;               //몬스터의 방어력
+    
+    public int damage;                  //몬스터의 데미지
+    
+    public float offense;               //몬스터의 공격력
+    
+    public float speed;                 //몬스터의 이동속도
+    
+    public float knockbackDefense;      //몬스터의 넉백 저항
 }
