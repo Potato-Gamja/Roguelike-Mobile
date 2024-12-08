a5337c027213ef12a5aae8b737e5c14b97659763
Potato-Gamja <90387670+Potato-Gamja@users.noreply.github.com>
2024-12-08 18:20:20 +0900

    Update PlayerData.cs

--- Scripts/DataScript/PlayerData.cs
+++ Scripts/DataScript/PlayerData.cs
@@ -7,29 +7,29 @@ using UnityEngine;
 
 public class PlayerData : ScriptableObject
 {
-    public float baseHp;
-    public float maxHp;
-    public float hp;
+    public float baseHp;                //플레이어의 체력
+    public float maxHp;                 
+    public float hp;                    
 
-    public float baseDefense;
-    public float defense;
+    public float baseDefense;           //플레이어의 방어력
+    public float defense;               
 
-    public float baseOffense;
-    public float offense;
+    public float baseOffense;           //플레이어의 공격력
+    public float offense;               
 
-    public float baseSpeed;
+    public float baseSpeed;             //플레이어의 이동속도
     public float speed;
 
-    public float baseAttackDelay;
+    public float baseAttackDelay;       //플레이어의 공격속도
     public float attackDelay;
 
-    public bool baseIsTrueDamage;
+    public bool baseIsTrueDamage;       //플레이어의 고정피해 여부
     public bool isTrueDamage;
 
-    public float baseCritical;
+    public float baseCritical;          //플레이어의 치명타 확률
     public float critical;
 
-    public float exp;
-    public float[] exps;
-    public Vector3 scale;
+    public float exp;                   //플레이어의 경험치
+    public float[] exps;                //플레이어의 레벨 별 필요 경험치
+    public Vector3 scale;               //플레이어의 크기
 }
