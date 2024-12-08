2f41ed62e02b524d5559f9dff0e269be16ed3f2e
Potato-Gamja <90387670+Potato-Gamja@users.noreply.github.com>
2024-12-08 18:10:52 +0900

    Update WeaponData.cs

--- Scripts/DataScript/WeaponData.cs
+++ Scripts/DataScript/WeaponData.cs
@@ -4,6 +4,7 @@ using Unity.VisualScripting;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "WeaponData", menuName = "Scriptble Object/WeaponData")]
+//에셋메뉴를 만들어 무기 데이터를 관리
 
 public class WeaponData : ScriptableObject
 {
@@ -21,6 +22,7 @@ public class WeaponData : ScriptableObject
 
     public bool baseIsKnock;
     public bool isKnock;
+    
     public float baseKnockback;
     public float knockback;
 
