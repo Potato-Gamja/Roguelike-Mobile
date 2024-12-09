90c584be865631dad6cfb7c1af515c217c180cfd
Potato-Gamja <90387670+Potato-Gamja@users.noreply.github.com>
2024-12-09 18:54:10 +0900

    Update LoadingSceneScript.cs

--- Scripts/LoadingSceneScript.cs
+++ Scripts/LoadingSceneScript.cs
@@ -5,44 +5,44 @@ using UnityEngine.UI;
 
 public class LoadingSceneScript : MonoBehaviour
 {
-    public static string nextScene;
-    [SerializeField] Image progressBar;
+    public static string nextScene;                    //다음 씬 화면의 문자열을 static으로 선언
+    [SerializeField]
+    Image loadingBar;                                 //로딩 게이지바
 
     private void Start()
     {
-        StartCoroutine(LoadScene());
+        StartCoroutine(LoadScene());                   //코루틴으로 로딩씬 함수 실행
     }
 
     public static void LoadScene(string sceneName)
-    {
-        nextScene = sceneName;
-        SceneManager.LoadScene("LoadScene");
+    {    
+        nextScene = sceneName;                         //다음씬 문자열에 전달 받은 씬이름 값을 대입
+        SceneManager.LoadScene("LoadScene");           //로드씬 실행
     }
 
     IEnumerator LoadScene()
     {
-        yield return null;
-        AsyncOperation op = SceneManager.LoadSceneAsync(nextScene);
-        op.allowSceneActivation = false;
-        float timer = 0.0f;
-        while (!op.isDone)
+        AsyncOperation ao = SceneManager.LoadSceneAsync(nextScene);
+        ao.allowSceneActivation = false;
+        float time = 0.0f;
+        while (!ao.isDone)                                                                        //동작이 준비가 안 되어 있으면 반복
         {
             yield return null;
-            timer += Time.deltaTime;
-            if (op.progress < 0.9f)
+            time += Time.deltaTime;
+            if (ao.progress < 0.9f)
             {
-                progressBar.fillAmount = Mathf.Lerp(progressBar.fillAmount, op.progress, timer);
-                if (progressBar.fillAmount >= op.progress)
+                loadingBar.fillAmount = Mathf.Lerp(loadingBar.fillAmount, ao.progress, time);   //선형보간으로 포
+                if (loadingBar.fillAmount >= ao.progress)
                 {
-                    timer = 0f;
+                    time = 0f;
                 }
             }
             else
             {
-                progressBar.fillAmount = Mathf.Lerp(progressBar.fillAmount, 1f, timer);
-                if (progressBar.fillAmount == 1.0f)
+                loadingBar.fillAmount = Mathf.Lerp(loadingBar.fillAmount, 1f, time);
+                if (loadingBar.fillAmount == 1.0f)
                 {
-                    op.allowSceneActivation = true;
+                    ao.allowSceneActivation = true;
                     yield break;
                 }
             }
