e1f29811e26c4714be26e99550828e433396f36c
Potato-Gamja <90387670+Potato-Gamja@users.noreply.github.com>
2024-12-09 18:36:14 +0900

    Update WarnScript.cs

--- Scripts/Monster/WarnScript.cs
+++ Scripts/Monster/WarnScript.cs
@@ -4,23 +4,11 @@ public class WarnScript : MonoBehaviour
 {
     MonManager monManager;
 
-    GameObject[] monPrefabs_1;
-    GameObject[] monPrefabs_2;
-    GameObject[] monPrefabs_3;
-    GameObject[] monPrefabs_4;
-
-    Transform trans;
-
-    //int mCount;
-    public float time;
-    int count;
-    int ran;
-    bool type = false;
-
-    float posX = 0;
-    float posY = 0;
-
-    public float warnTime;
+    public float time;        //타이머
+    public float warnTime;    //경고 시간
+    
+    float posX = 0;           //좌표 X
+    float posY = 0;           //좌표 Y
 
     private void Awake()
     {
@@ -32,23 +20,22 @@ public class WarnScript : MonoBehaviour
     {
         time += Time.deltaTime;
 
-        if (time >= warnTime)
+        if (time >= warnTime)                                                          //경고 시간에 달성 시 실행
         {
-            monManager.EnableMons(posX, posY);
-            gameObject.SetActive(false);
+            monManager.EnableMons(posX, posY);                                         //몬스터 생성 함수 실행
+            gameObject.SetActive(false);                                               //경고 오브젝트 비활성화
         }
     }
 
     public void Set(float pos1, float pos2, float warntime)
     {
-        transform.rotation = Quaternion.Euler(0f, 0f, Random.Range(-20f, 20f));
-        posX = pos1;
-        posY = pos2;
-        warnTime = warntime;
-        time = 0;
-        type = true;
+        transform.rotation = Quaternion.Euler(0f, 0f, Random.Range(-20f, 20f));        //경고 오브젝트의 회전값을 랜덤으로 하여 동일한 모양의 경고만 나오는 것을 방지
+        posX = pos1;                                                                   //좌표 X값
+        posY = pos2;                                                                   //좌표 Y값
+        warnTime = warntime;                                                           //경고 시간
+        time = 0;                                                                      //타이머 0으로 초기화
 
-        if (monManager.mCount >= monManager.monPool)
+        if (monManager.mCount >= monManager.monPool)                                   //경고와 몬스터의 인덱스 값이 초과 시 0으로 초기화
             monManager.mCount = 0;
         if (monManager.wCount >= monManager.warnPool)
             monManager.wCount = 0;
