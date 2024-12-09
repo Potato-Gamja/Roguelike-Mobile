f266bb2c0c61781e9200d87b55fec2b4e3d390f6
Potato-Gamja <90387670+Potato-Gamja@users.noreply.github.com>
2024-12-09 21:16:55 +0900

    Update WeaponScript.cs

--- Scripts/WeaponScript.cs
+++ Scripts/WeaponScript.cs
@@ -5,24 +5,22 @@ public class WeaponScript : MonoBehaviour
     [SerializeField]
     GameManager gameManager;
 
-    public GameObject missile;
-    public GameObject[] missilePrefab;
-    public GameObject missileGroup;
+    public GameObject missile;                //미사일 게임오브젝트
+    public GameObject[] missilePrefab;        //미사일 풀링 배열
+    public GameObject missileGroup;           //미사일 부모 오브젝트
 
-    public int weaponPool;
+    public int weaponPool;                    //미사일 풀링 수
 
     void Awake()
     {
-        missile = gameManager.weapon[gameManager.weaponType];
-
         missilePrefab = new GameObject[weaponPool];
 
         for (int i = 0; i < missilePrefab.Length; i++)
         {
-            GameObject gameObject = Instantiate(missile);
-            gameObject.transform.parent = missileGroup.transform;
-            missilePrefab[i] = gameObject;
-            gameObject.SetActive(false);
+            GameObject gameObject = Instantiate(missile);            //미사일 오브젝트 생성
+            gameObject.transform.parent = missileGroup.transform;    //미사일 오브젝트의 부모 변경
+            missilePrefab[i] = gameObject;                           //미사일 풀링 배열에 게임오브젝트 대입
+            gameObject.SetActive(false);                             //미사일 비활성화
         }
 
     }
