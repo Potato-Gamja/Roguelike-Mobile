49a6fbd15ae8a9feb1498569e2b1bd61dcb91e04
Potato-Gamja <90387670+Potato-Gamja@users.noreply.github.com>
2024-12-09 20:17:55 +0900

    Update PlayerScript.cs

--- Scripts/Player/PlayerScript.cs
+++ Scripts/Player/PlayerScript.cs
@@ -16,7 +16,7 @@ public class PlayerScript : MonoBehaviour
     SpriteRenderer spriter;
     Transform trans;
     [SerializeField]
-    Transform staff;
+    Transform staff;                    //미사일의 공격 위치
 
     public GameObject hpBar;            //체력바
     public GameObject hpBarPos;         //체력바 위치
@@ -48,8 +48,8 @@ public class PlayerScript : MonoBehaviour
     int weaponCount = 0;                //미사일 인덱스 값
     
     float attackTime_Missile = 0;       //미사일 공격 타이머
-    float attackTime_Laser = 0;         //레이저 공격 타이머
-    float attackTime_Sword = 0;         //마법검 공격 타이머
+    public float attackTime_Laser = 0;         //레이저 공격 타이머
+    public float attackTime_Sword = 0;         //마법검 공격 타이머
 
     [SerializeField]
     CircleCollider2D[] missileCol;      //미사일의 콜라이더
@@ -128,91 +128,98 @@ public class PlayerScript : MonoBehaviour
             isSword = false;                                                                              //마법검 공격 여부
 
         }
-        if (attackTime_Laser >= (levelUpScript.laserData.attackDelay / 100) * attackDelay && !isLaser)
+        if (attackTime_Laser >= (levelUpScript.laserData.attackDelay / 100) * attackDelay && !isLaser)    //레이저 쿨타임과 타이머 비교
         {
-            laserWeapon.hitCount = levelUpScript.laserData.count;
-            isLaser = true;
-            //laserWeapon.LaserTrans();
-            laserEnd.Retarget();
-            laserBeam.SetActive(true);
-            soundManager.PlayLaserSound();
+            laserWeapon.hitCount = levelUpScript.laserData.count;                                         //레이저의 최대 타격 회수 대입
+            isLaser = true;                                                                               //레이저 공격 여부
+            laserEnd.Retarget();                                                                          //레이저의 타겟 설정
+            laserBeam.SetActive(true);                                                                    //레이저빔 활성화
+            soundManager.PlayLaserSound();                                                                //레이저 사운드 재생
         }
     }
 
-    void Move()
+    void Move()                                //플레이어 이동
     {
-        float x = joy.Horizontal;
-        float y = joy.Vertical;
+        float x = joy.Horizontal;              //조이스틱의 수평 값 대입
+        float y = joy.Vertical;                //조이스틱의 수직 값 대입
 
-        vec = new Vector3(x, y, 0);
+        vec = new Vector3(x, y, 0);            //입력값 x, y 대입
 
-        if (x < 0)
+        if (x < 0)                                                                    //왼쪽으로 이동 시
         {
-            animator.SetBool("Run", true);
-            trans.localScale = new Vector3(scale.x * -1, scale.y, scale.z);
-            hpBar.transform.localScale = new Vector3(-0.02f, 0.026f, 0.02f);
-            laserOrb.transform.position = new Vector3(transform.position.x - 0.75f, transform.position.y + 0.75f, transform.position.z);
+            animator.SetBool("Run", true);                                            //달리기 애니메이션 실행
+            trans.localScale = new Vector3(scale.x * -1, scale.y, scale.z);           //플레이어가 왼쪽을 보게 스케일 값 변경
+            hpBar.transform.localScale = new Vector3(-0.02f, 0.026f, 0.02f);          //체력바 스케일 값 변경
+            laserOrb.transform.position = new Vector3(transform.position.x - 0.75f,   //레이저 오브의 위치와 스케일 값 변경
+                                                      transform.position.y + 0.75f,
+                                                      transform.position.z);
             laserOrb.transform.localScale = new Vector3(-0.8f, 0.8f, 1);
-            floor.transform.localScale = new Vector3(-levelUpScript.floorData.range, levelUpScript.floorData.range, levelUpScript.floorData.range);
+            floor.transform.localScale = new Vector3(-levelUpScript.floorData.range,  //장판의 스케일 값 조정
+                                                     levelUpScript.floorData.range,
+                                                     levelUpScript.floorData.range);
             
         }
-        else if (x > 0)
+        else if (x > 0)                                                               //오른쪽으로 이동 시
         {
-            animator.SetBool("Run", true);
-            trans.localScale = new Vector3(scale.x, scale.y, scale.z);
+            animator.SetBool("Run", true); 
+            trans.localScale = new Vector3(scale.x, scale.y, scale.z);  
             hpBar.transform.localScale = new Vector3(0.02f, 0.026f, 0.02f);
-            laserOrb.transform.position = new Vector3(transform.position.x - 0.75f, transform.position.y + 0.75f, transform.position.z);
+            laserOrb.transform.position = new Vector3(transform.position.x - 0.75f,
+                                                      transform.position.y + 0.75f,
+                                                      transform.position.z);
             laserOrb.transform.localScale = new Vector3(0.8f, 0.8f, 1);
-            floor.transform.localScale = new Vector3(levelUpScript.floorData.range, levelUpScript.floorData.range, levelUpScript.floorData.range);
+            floor.transform.localScale = new Vector3(levelUpScript.floorData.range,
+                                                     levelUpScript.floorData.range,
+                                                     levelUpScript.floorData.range);
             
         }
-        else if (x == 0 || y == 0)
+        else if (x == 0 || y == 0)                                                   //입력값이 없을 경우
         {
-            animator.SetBool("Run", false);
+            animator.SetBool("Run", false);                                          //달리기 애니메이션 중지, idle 애니메이션으로
         }
 
-        transform.position += (baseSpeed / 100 * speed) * Time.deltaTime * vec;
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x, minPos.x, maxPos.x),
+        transform.position += (baseSpeed / 100 * speed) * Time.deltaTime * vec;                    //조이스틱의 입력값에 속도를 곱한 만큼 이동
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, minPos.x, maxPos.x),    //클램프를 사용하여 최소, 최대 이동범위 설정
                                          Mathf.Clamp(transform.position.y, minPos.y, maxPos.y),
                                          0);
     }
 
-    void Attack_Missile()
+    void Attack_Missile()                                                                                             //미사일 공격
     {
-        if (!scanner.nearestTarget)
+        if (!scanner.nearestTarget)                                                                                   //스캐너에 타겟이 없을 경우 리턴
             return;
 
-        Vector3 targetPos = scanner.nearestTarget.position;
-        Vector3 dir = targetPos - staff.transform.position;
-        dir = dir.normalized;
+        Vector3 targetPos = scanner.nearestTarget.position;                                                           //타겟 위치
+        Vector3 dir = targetPos - staff.transform.position;                                                           //타겍과 공격 위치인 지팡이와의 거리 비교
+        dir = dir.normalized;                                                                                         //방향 벡터터
 
-        if (weaponScript.missilePrefab[weaponCount].activeSelf == false)
+        if (weaponScript.missilePrefab[weaponCount].activeSelf == false)                                              //인덱스 값의 미사일이 비활성화일 경우 실행
         {
-            weaponScript.missilePrefab[weaponCount].transform.position = staff.transform.position;
-            weaponScript.missilePrefab[weaponCount].SetActive(true);
-            soundManager.PlayMissileSound();
-            weaponScript.missilePrefab[weaponCount].transform.rotation = Quaternion.FromToRotation(Vector3.up, dir);
-            missileCol[weaponCount].enabled = true;
-            missileWeapon[weaponCount].hitCount = levelUpScript.missileData.count;
+            weaponScript.missilePrefab[weaponCount].transform.position = staff.transform.position;                    //미사일을 지팡이의 공격 위치로 이동
+            weaponScript.missilePrefab[weaponCount].SetActive(true);                                                  //미사일 활성화
+            soundManager.PlayMissileSound();                                                                          //미사일 사운드 재생
+            weaponScript.missilePrefab[weaponCount].transform.rotation = Quaternion.FromToRotation(Vector3.up, dir);  //타겟의 방향쪽으로 미사일 회전
+            missileCol[weaponCount].enabled = true;                                                                   //미사일 콜라이더 활성화
+            missileWeapon[weaponCount].hitCount = levelUpScript.missileData.count;                                    //미사일의 타격 수
             
-            attackTime_Missile = 0;
+            attackTime_Missile = 0;                                                                                   //미사일의 타이머 0으로 초기화
 
-            weaponCount++;
+            weaponCount++;                                                                                            //인덱스 값 증가
 
-            if (weaponCount >= weaponScript.weaponPool)
+            if (weaponCount >= weaponScript.weaponPool)                                                               //인덱스 값 초과 시 0으로 초기화
                 weaponCount = 0;
         }
     }
 
-    public void Attack_Sword()
+    public void Attack_Sword()                                                                                        //마법검 공격
     {
         if (isSword)
         {
-            levelUpScript.sword[levelUpScript.swordCount].SetActive(true);
-            swords[levelUpScript.swordCount].swordAni.SetBool("isSword", true);
-            levelUpScript.sword[levelUpScript.swordCount].transform.position = gameObject.transform.position;
+            levelUpScript.sword[levelUpScript.swordCount].SetActive(true);                                            //마법검 활성화
+            swords[levelUpScript.swordCount].swordAni.SetBool("isSword", true);                                       //마법검 애니메이션 실행
+            levelUpScript.sword[levelUpScript.swordCount].transform.position = gameObject.transform.position;         //마법검의 위치 조정
             
-            soundManager.PlaySwordSound();
+            soundManager.PlaySwordSound();                                                                            //마법검 사운드 재생
         }
     }
 }
