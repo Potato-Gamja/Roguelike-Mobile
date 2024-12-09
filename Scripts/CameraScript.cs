da3b3344b617f0c7cab8f9c19cb87c8dca5aff68
Potato-Gamja <90387670+Potato-Gamja@users.noreply.github.com>
2024-12-09 18:42:49 +0900

    Update CameraScript.cs

--- Scripts/CameraScript.cs
+++ Scripts/CameraScript.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
-    public Transform player;
+    public Transform player;                 //플레이어의 트랜스폼
 
-    public float speed = 0.1f;
+    public float speed = 0.1f;               //카메라의 이동속도
 
-    Vector3 camPos;
+    Vector3 camPos;                          //카메라의 위치
 
-    public Vector2 minCamPos;
-    public Vector2 maxCamPos;
+    public Vector2 minCamPos;                //카메라의 최소 이동 범위
+    public Vector2 maxCamPos;                //카메라의 최대 이동 범위
 
     void Start()
     {
@@ -21,32 +21,11 @@ public class CameraScript : MonoBehaviour
         if (player == null)
             return;
 
-        Vector3 pos = Vector3.Lerp(transform.position, player.position, speed);
+        Vector3 pos = Vector3.Lerp(transform.position, player.position, speed);                         //부드러운 이동을 위한 선형보간
 
-        transform.position = new Vector3(Mathf.Clamp(pos.x, minCamPos.x, maxCamPos.x) + camPos.x,
+        transform.position = new Vector3(Mathf.Clamp(pos.x, minCamPos.x, maxCamPos.x) + camPos.x,       //클램프를 이용하여 최소, 최대 범위를 설정하여 값이 넘지 않도록 하기
                                          Mathf.Clamp(pos.y, minCamPos.y, maxCamPos.y) + camPos.y,
                                          -10f + camPos.y);
     }
 
-    public void SetResolution()
-    {
-        int setWidth = 1080;
-        int setHeight = 1920;
-
-        int deviceWidth = Screen.width;
-        int deviceHeight = Screen.height;
-
-        Screen.SetResolution(setWidth, (int)(((float)deviceHeight / deviceWidth) * setWidth), true);
-
-        if ((float)setWidth / setHeight < (float)deviceWidth / deviceHeight)
-        {
-            float newWidth = ((float)setWidth / setHeight) / ((float)deviceWidth / deviceHeight);
-            Camera.main.rect = new Rect((1f - newWidth) / 2f, 0f, newWidth, 1f);
-        }
-        else
-        {
-            float newHeight = ((float)deviceWidth / deviceHeight) / ((float)setWidth / setHeight);
-            Camera.main.rect = new Rect(0f, (1f - newHeight) / 2f, 1f, newHeight);
-        }
-    }
 }
